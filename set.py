9665575b029e9d3de1a7f6d13c46db59ac97b8a7
Simmi141 <142918333+Simmi141@users.noreply.github.com>
2024-10-06 20:01:59 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,12 @@
+#ordered(U can't be sure in which order items will appear) and so unindexed
+s=("apple","banana","cherry")
+print(s)
+
+#does not allow duplicate members
+sd={"apple","apple","banana","cherry"}
+print(sd)
+
+#unchangeable(no output)
+z=("apple","banana","cherry")
+z[1]="kiwi"
+print(z)
